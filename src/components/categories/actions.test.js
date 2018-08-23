80bb86fc59d9a469f8e80d3f803255303db0843e
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-23 07:54:12 -0700

    Make test for adding an action

--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -2,7 +2,7 @@ import { CATEGORY_LOAD } from './reducers';
 import data from '../../services/category-data';
 import { 
   load,
-  // add,
+  add,
   // update,
   // remove
 } from './actions';
@@ -14,5 +14,11 @@ describe('Categories actions', () => {
       type: CATEGORY_LOAD,
       payload: data
     });
+  });
+
+  it('adds an animal with a generated short id', () => {
+    const newCategory = { timestamp: new Date(), name: 'dining', budget: 150 };
+    const action = add(newCategory);
+    expect(action.key).toBeDefined();
   });
 });
