73ce560dfefc82b326f5be53c4be867b8ec62829
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-23 09:20:53 -0700

    Adding mock-data and actions

---
+++ src/components/categories/categories-data.js
@@ -0,0 +1,9 @@
+
+export default [
+  // eslint-disable-next-line
+  { key: "-LKTQHx7y2E_ZNFUEf-b", "name": "Automobile", "budget": 800 }, 
+  // eslint-disable-next-line
+  { key: "-LKTi4U-7iQyGe9m1Zy5", "name": "Groceries", "budget": 500 },
+  // eslint-disable-next-line
+  { key: "-LKTi4U-7iQyGe9m1Zy5", "name": "Entertainment", "budget": 500 }
+];
