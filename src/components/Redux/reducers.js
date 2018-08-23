6248c9176b82d381347f367d2d031271287bab07
MariahAdams <mariah.adams23@yahoo.com>
2018-08-23 08:42:50 -0700

    reducer test for CATEGORY_ADD passing

--- src/components/Redux/reducers.js
+++ src/components/Redux/reducers.js
@@ -1,12 +1,17 @@
 export const CATEGORY_LOAD = 'CATEGORY_LOAD';
-// // export const CATEGORY_ADD = 'CATEGORY_ADD';
-// // export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
-// // export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
+export const CATEGORY_ADD = 'CATEGORY_ADD';
+// export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
+// export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
 
 export function categories(state = [], { type, payload }) {
   switch(type) {
     case CATEGORY_LOAD:
       return payload;
+    case CATEGORY_ADD:
+      return [
+        ...state,
+        payload
+      ];
     default:  
       return state; 
   }
--- src/components/Redux/reducers.test.js
+++ src/components/Redux/reducers.test.js
@@ -1,9 +1,9 @@
 import { 
   categories,
   CATEGORY_LOAD,
-  /* CATEGORY_ADD,
-  CATEGORY_UPDATE,
-  CATEGORY_REMOVE */  } from './reducers';
+  CATEGORY_ADD,
+  /* CATEGORY_UPDATE, */
+  /* CATEGORY_REMOVE */  } from './reducers';
 
 describe('Reducers', () => {
 
@@ -13,7 +13,7 @@ describe('Reducers', () => {
   });
 
   it('loads a category', () => {
-    const payload = [{}, {}];
+    const payload = [{}, {}, {}];
 
     const state = categories([], {
       type: CATEGORY_LOAD,
@@ -21,5 +21,18 @@ describe('Reducers', () => {
     });
 
     expect(state).toBe(payload);
+  });
+
+  it('adds categories', () => {
+    const category1 = { name: 'gas' };
+    const category2 = { name: 'rent' };
+    const category3 = { name: 'food' };
+
+    const state = categories([category1, category2], {
+      type: CATEGORY_ADD,
+      payload: category3
+    });
+
+    expect(state).toEqual([category1, category2, category3]);
   });
 });
