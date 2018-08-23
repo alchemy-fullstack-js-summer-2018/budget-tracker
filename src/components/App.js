08ddbb5adf25c3ab3b09106a4c60fcb172d497b0
carriecarter <carrie@carrie-carter.com>
2018-08-22 18:03:54 -0700

    wheeew need to eat foods

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import styles from './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div className={styles.App}>
+          <header>
+            <Header/>
+          </header>
+        </div>
+      </Router>
+    )
+  }
+}
