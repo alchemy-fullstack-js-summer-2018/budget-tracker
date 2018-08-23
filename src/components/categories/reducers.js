8c7ce9c34f61fd26e9292909141339c47bfed898
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-23 09:37:08 -0700

    Load categories reducer test successful

--- src/components/categories/reducers.js
+++ src/components/categories/reducers.js
@@ -1,12 +1,12 @@
-export const CATEGORY_LOAD = 'CATEGORY_ADD';
+export const CATEGORY_LOAD = 'CATEGORY_LOAD';
 export const CATEGORY_ADD = 'CATEGORY_ADD';
-export const CATEGORY_UPDATE = 'CATEGORY_ADD';
-export const CATEGORY_REMOVE = 'CATEGORY_ADD';
+export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
+export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
 
 export function categories(state = [], { type, payload }) {
   switch(type) {
-    // case CATEGORY_LOAD:
-    //   return payload;
+    case CATEGORY_LOAD:
+      return payload;
     case CATEGORY_ADD:
       return [
         ...state,
--- src/components/categories/reducers.test.js
+++ src/components/categories/reducers.test.js
@@ -13,6 +13,21 @@ describe('Categories Reducers', () => {
     expect(state).toEqual([]);
   });
 
+  it('Loads categories', () => {
+    const payload = [
+      { name: 'Automobile', budget: 800 },
+      { name: 'Groceries', budget: 500 },
+      { name: 'Entertainment', budget: 500 }
+    ];
+
+    const state = categories([], {
+      type: CATEGORY_LOAD,
+      payload
+    });
+
+    expect(state).toBe(payload);
+  });
+  
   it('Adds a category', () => {
     const category1 = { name: 'Automobile' };
     const category2 = { name: 'Food & Dining' };
