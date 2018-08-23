e458dd41cc0f7e2d326b477c3a8cae43e4e6bf80
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-22 22:15:04 -0700

    Add shortid package, begin to write store, and add mock data

---
+++ src/services/category-data.js
@@ -0,0 +1,17 @@
+[
+  {
+    timestamp: new Date(),
+    name: 'entertainment',
+    budget: 300
+  },
+  {
+    timestamp: new Date(),
+    name: 'food',
+    budget: 500
+  },
+  {
+    timestamp: new Date(),
+    name: 'transportation',
+    budget: 200
+  }
+];
