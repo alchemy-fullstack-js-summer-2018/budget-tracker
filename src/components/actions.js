d4fdb68f4fb7c959d504d5341427e4238d41bb79
Mark Myers <markalope@gmail.com>
2018-08-23 09:51:51 -0700

    fixed an issue in jest config and now two tests are passing

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,4 +1,9 @@
-import { CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_UPDATE, CATEGORY_REMOVE } from './reducers.js';
+import { 
+  CATEGORY_LOAD, 
+  CATEGORY_ADD 
+  // CATEGORY_UPDATE, 
+  // CATEGORY_REMOVE 
+} from './reducers.js';
 import data from './categories-data';
 import shortid from 'shortid';
 
@@ -16,12 +21,12 @@ export const add = category => {
   };
 };
 
-export const update = category => ({
-  type: CATEGORY_UPDATE,
-  payload: category
-});
+// export const update = category => ({
+//   type: CATEGORY_UPDATE,
+//   payload: category
+// });
 
-export const remove = id => ({
-  type: CATEGORY_REMOVE,
-  payload: id
-});
+// export const remove = id => ({
+//   type: CATEGORY_REMOVE,
+//   payload: id
+// });
--- src/components/reducers.test.js
+++ src/components/reducers.test.js
@@ -1,9 +1,9 @@
 import {
   categories,
-  CATEGORY_LOAD,
-  CATEGORY_ADD,
-  CATEGORY_UPDATE,
-  CATEGORY_REMOVE
+  CATEGORY_LOAD
+  // CATEGORY_ADD
+  // CATEGORY_UPDATE,
+  // CATEGORY_REMOVE
 } from './reducers';
 
 describe('categories reducers', () => {
@@ -11,6 +11,17 @@ describe('categories reducers', () => {
   it('initializes to an empty array', () => {
     const state = categories(undefined, {});
     expect (state).toEqual([]);
+  });
+
+  it('loads categories', () => {
+    const payload = [{}, {}, {}];
+
+    const state = categories([], {
+      type: CATEGORY_LOAD,
+      payload
+    });
+
+    expect(state).toBe(payload);
   });
 
 });
