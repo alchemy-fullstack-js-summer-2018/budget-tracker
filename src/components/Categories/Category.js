8d5eaf27dac11ab182171c26b0b2a48be1ead418
MariahAdams <mariah.adams23@yahoo.com>
2018-08-23 11:06:39 -0700

    updating Category component

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import CategoryForm from './CategoryForm';
 import CategoryItem from './CategoryItem';
+//TODO: update -- don't forget to connect
+//TODO: edit
 
 class Category extends Component {
+
+  static propTypes = {
+    category: PropTypes.object.isRequired
+  };
   
   render() { 
+    const { category } = this.props;
     
     return (
       <li style={{ color: 'green' }}>
         <h4>Category Component</h4>
 
-        <CategoryForm/>
+        <CategoryForm
+          category={category}
+        />
 
-        <CategoryItem />
+        <CategoryItem 
+          category={category}
+        />
       </li>
     );
   }
 }
  
-export default Category;
+export default connect(
+  null
+)(Category);
