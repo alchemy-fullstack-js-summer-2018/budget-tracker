2ab3d57427b84675c33e3f80f8c4f4298c3a048b
Mario Quintana <pyrros2000@gmail.com>
2018-08-22 17:42:05 -0700

    All reducers test passing

--- src/components/demo/reducers.test.js
+++ src/components/demo/reducers.test.js
@@ -1,4 +1,4 @@
-import { count } from './reducers';
+import { count, INCREMENT, DECREMENT, CHANGE } from './reducers';
 
 describe.only('Demo reducers', () => {
 
@@ -6,4 +6,20 @@ describe.only('Demo reducers', () => {
     const state = count(undefined, {});
     expect(state).toBe(0);
   });
+
+  it('Increments the count', () => {
+    const state = count(0, { type: INCREMENT });
+    expect(state).toBe(1);
+  });
+
+  it('Decrements the count', () => {
+    const state = count(5, { type: DECREMENT });
+    expect(state).toBe(4);
+  });
+
+  it('Changes by amount', () => {
+    const state = count(5, { type: CHANGE, payload: 3 });
+    expect(state).toBe(8);
+  });
+
 });
