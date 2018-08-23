ad89f8ae789009be717ad0361f20e1de47de9312
Mario Quintana <pyrros2000@gmail.com>
2018-08-23 08:41:24 -0700

    Second categoryForm test passing, more form tests needed

--- src/components/categories/CategoryForm.test.js
+++ src/components/categories/CategoryForm.test.js
@@ -12,6 +12,53 @@ describe('Category form', () => {
 
     const wrapper = mount(<CategoryForm onComplete={handleComplete}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
+
+    const category = {
+      name: 'Car',
+      budget: 5000
+    };
+
+    wrapper.find('input[name="name"]').simulate('change', {
+      target: {
+        name: 'name',
+        value: category.name
+      }
+    });
+
+    wrapper.find('input[name="budget"]').simulate('change', {
+      target: {
+        name: 'budget',
+        value: category.budget
+      }
+    });
+
+    wrapper.find('button').simulate('submit');
+
+    const calls = handleComplete.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual(category);
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    return promise  .then(() => {
+      wrapper.update();
+      expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+
   });
-  
+
+  it('Renders edit if there is category prop', () => {
+    const handleComplete = jest.fn();
+    const promise = Promise.resolve();
+    handleComplete.mockReturnValueOnce(promise);
+    const handleCancel = jest.fn();
+
+    const category = { key: 'abc', name: 'car', budget: 5000 };
+
+    const wrapper = mount(<CategoryForm onComplete={handleComplete} onCancel={handleCancel} category={category}/>);
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+  });
+
 });
