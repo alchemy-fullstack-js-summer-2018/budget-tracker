ac315383c886955deca8e083e92580f2673504a2
Mario Quintana <pyrros2000@gmail.com>
2018-08-22 22:06:21 -0700

    Created mock data

---
+++ src/components/categories/categories-data.js
@@ -0,0 +1,8 @@
+export default [
+  // eslint-disable-next-line
+  { key: "123", timestamp: new Date(), name: "car", budget: 5000 }, 
+  // eslint-disable-next-line
+  { key: "456", timestamp: new Date(), name: "bike", budget: 100 },
+  // eslint-disable-next-line
+  { key: "789", timestamp: new Date(), name: "pc", budget: 800 }
+];
