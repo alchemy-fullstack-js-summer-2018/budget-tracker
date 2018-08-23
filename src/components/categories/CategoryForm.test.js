c58ca507c8bdad5e4437d9899ebf58ee1be74e2c
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-23 11:14:31 -0700

    Finish writing first test for CategoryForm

--- src/components/categories/CategoryForm.test.js
+++ src/components/categories/CategoryForm.test.js
@@ -20,5 +20,18 @@ describe('Category form', () => {
         value: category.name
       }
     });
+
+    wrapper.find('input[name="budget"]').simulate('change', {
+      target: {
+        name: 'budget',
+        value: category.budget
+      }
+    });
+
+    wrapper.find('button').simulate('submit');
+
+    const calls = handleComplete.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual(category);
   });
 });
