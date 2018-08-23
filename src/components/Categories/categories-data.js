73b344b2c13f75cc55452674ca399d13d5b7729d
MariahAdams <mariah.adams23@yahoo.com>
2018-08-23 16:50:09 -0700

    updating mock data

--- src/components/Categories/categories-data.js
+++ src/components/Categories/categories-data.js
@@ -1,4 +1,32 @@
 export default [
-  { key: '-LKZT8AlXZOMAM40VKPD', budget: 100, id: 'abc123', name: 'food', timestamp: '2018-08-22' },
-  { key: '-LKZTGF4mchHm10LGZFP', budget: 100, id: 'efg456', name: 'gas', timestamp: '2018-08-21' }
+  { key: '-LKZT8AlXZOMAM40VKPD', 
+    budget: 100, 
+    id: 'abc123', 
+    name: 'food', 
+    timestamp: '2018-08-22', 
+    expense: [
+      {
+        id: 'yum123',
+        categoryId: 'abc123',
+        timestamp: '2018-08-22',
+        name: 'yumm',
+        price: 5 
+      }
+    ] 
+  },
+  { key: '-LKZTGF4mchHm10LGZFP', 
+    budget: 500, 
+    id: 'efg456', 
+    name: 'gas', 
+    timestamp: '2018-08-21', 
+    expense: [
+      {
+        id: 'boo123',
+        categoryId: 'efg456',
+        timestamp: '2018-08-22',
+        name: 'regular fill',
+        price: 50
+      }
+    ] 
+  }
 ];
