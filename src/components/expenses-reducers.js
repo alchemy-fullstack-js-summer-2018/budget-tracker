fa3e42aef3731e51428c8ebb602368de2c7bba60
Mark Myers <markalope@gmail.com>
2018-08-24 11:48:41 -0700

    remove expense load from reducers

--- src/components/expenses-reducers.js
+++ src/components/expenses-reducers.js
@@ -1,12 +1,9 @@
-export const EXPENSE_LOAD = 'EXPENSE_LOAD';
 export const EXPENSE_ADD = 'EXPENSE_ADD';
 export const EXPENSE_UPDATE = 'EXPENSE_UPDATE';
 export const EXPENSE_REMOVE = 'EXPENSE_REMOVE';
 
 export function expenses(state = [], { type, payload }) {
   switch(type) {
-    case EXPENSE_LOAD:
-      return payload;
     case EXPENSE_ADD:
       return [
         ...state,
