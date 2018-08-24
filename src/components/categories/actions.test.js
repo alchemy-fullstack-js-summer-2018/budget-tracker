2aa0dcf351500f3e7d610a4459bc712ca17d3a46
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-24 08:42:25 -0700

    Action Tests Add & Update Passing

--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -1,12 +1,24 @@
-import { load, } from './actions';
+import { load, add, update } from './actions';
 import  data from './categories-data';
-import { CATEGORY_LOAD, } from './reducers';
+import { CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_UPDATE } from './reducers';
 
 describe('Category actions tests', () => {
   
-  it('Loads', () => {
+  it('Loads a category', () => {
     const action = load();
     expect(action).toEqual({ type: CATEGORY_LOAD, payload: data });
   });
 
+
+  it('Adds a category', () => {
+    const payload = {};
+    const action = add(payload);
+    expect(action).toEqual({ type: CATEGORY_ADD, payload: payload });
+  });
+
+  it('Updates a category', () => {
+    const action = update(9);
+    expect(action).toEqual({ type: CATEGORY_UPDATE,   payload: 9
+    });
+  });
 });  
