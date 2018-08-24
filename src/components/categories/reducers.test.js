a3a56dffcb5a55232a4f8336d4b9505f2ef2d6c2
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-24 08:23:02 -0700

    Reducers Tests Add, Update, Remove Passing

--- src/components/categories/reducers.test.js
+++ src/components/categories/reducers.test.js
@@ -1,10 +1,13 @@
 import { 
   categories, 
-  CATEGORY_LOAD
-
+  CATEGORY_LOAD,
+  CATEGORY_ADD,
+  CATEGORY_UPDATE,
+  CATEGORY_REMOVE
+  
 } from './reducers';
 
-describe('animals reducers', () => {
+describe('Categories reducers', () => {
 
   it('initialize to empty array', () => {
     const state = categories(undefined, {});
@@ -20,6 +23,47 @@ describe('animals reducers', () => {
     });
 
     expect(state).toBe(payload);
+  });
+
+  it('adds category', () => {
+    const category1 = { name: '1' };
+    const category2 = { name: '2' };
+    const category3 = { name: '3' };
+
+    const state = categories([category1, category2], { 
+      type: CATEGORY_ADD,
+      payload: category3
+    });
+
+    expect(state).toEqual([category1, category2, category3]);
+  });
+
+  it('Updates a category', () => {
+    const category1 = { key: '1', name: 'a' };
+    const category2 = { key: '2', name: 'b' };
+    const category3 = { key: '3', name: 'c' };
+
+    const updated = { key: '2', name: 'f' };
+
+    const state = categories([category1, category2, category3], {
+      type: CATEGORY_UPDATE,
+      payload: updated
+    });
+
+    expect(state).toEqual([category1, updated, category3]);
+  });
+
+  it('Removes a category', () => {
+    const category1 = { key: '1', name: 'a' };
+    const category2 = { key: '2', name: 'b' };
+    const category3 = { key: '3', name: 'c' };
+
+    const state = categories([category1, category2, category3], {
+      type: CATEGORY_REMOVE,
+      payload: '2'
+    });
+
+    expect(state).toEqual([category1, category3]);
   });
 
 });
