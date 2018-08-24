cd78b85ec283f8c24e64e2651894205ad2cce1d6
Mark Myers <markalope@gmail.com>
2018-08-24 10:52:11 -0700

    extending mock data to include expenses

---
+++ src/components/expenses-actions.js
@@ -0,0 +1,32 @@
+import { 
+  CATEGORY_LOAD, 
+  CATEGORY_ADD, 
+  CATEGORY_UPDATE,
+  CATEGORY_REMOVE 
+} from './category-reducers.js';
+import data from './categories/categories-data.js';
+import shortid from 'shortid';
+
+export const load = () => ({
+  type: CATEGORY_LOAD,
+  payload: data
+});
+
+export const add = category => {
+  category.key = shortid.generate();
+  category.timestamp = new Date();
+  return {
+    type: CATEGORY_ADD,
+    payload: category
+  };
+};
+
+export const update = category => ({
+  type: CATEGORY_UPDATE,
+  payload: category
+});
+
+export const remove = id => ({
+  type: CATEGORY_REMOVE,
+  payload: id
+});
