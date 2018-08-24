2e64c25da21ace7497b06a8153203959d33b72d6
MariahAdams <mariah.adams23@yahoo.com>
2018-08-23 17:27:32 -0700

    working on expense reducers test, add passing

--- src/components/Redux/expensesByCategory.js
+++ src/components/Redux/expensesByCategory.js
@@ -1,10 +1,14 @@
-export const EXPENSE_LOAD = 'EXPENSE_LOAD';
-// export const EXPENSE_ADD = 'EXPENSE_ADD';
+export const EXPENSE_ADD = 'EXPENSE_ADD';
 // export const EXPENSE_UPDATE = 'EXPENSE_UPDATE';
 // export const EXPENSE_REMOVE = 'EXPENSE_REMOVE';
 
-export function expenses(state = [], { type/* , payload */ }) {
+export function expenses(state = [], { type, payload }) {
   switch(type) {
+    case EXPENSE_ADD:
+      return [
+        ...state,
+        payload
+      ];
     default:
       return state;
   }
--- src/components/Redux/expensesByCategory.test.js
+++ src/components/Redux/expensesByCategory.test.js
@@ -1,9 +1,29 @@
-import { expenses } from './expensesByCategory';
+import { 
+  expenses,
+  EXPENSE_ADD } from './expensesByCategory';
+import { 
+/* categories, */
+/* CATEGORY_LOAD, */
+/*  CATEGORY_ADD, */
+/* CATEGORY_REMOVE */  } from './reducers';
 
 describe('Expense Reducers', () => {
 
   it('initializes to an empty array', () => {
     const state = expenses(undefined, {});
     expect(state).toEqual([]);
+  });
+
+  it('adds an expense', () => {
+    const expense1 = { name: '1' };
+    const expense2 = { name: '2' };
+    const expense3 = { name: '3' };
+
+    const state = expenses([expense1, expense2], {
+      type: EXPENSE_ADD,
+      payload: expense3
+    });
+
+    expect(state).toEqual([expense1, expense2, expense3]);
   });
 });
