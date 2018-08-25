f91fcfd7c3a4a6094ff729b95805293f1a87e92b
Rehmsy <sarahrehmer@gmail.com>
2018-08-25 10:59:07 -0700

    need to switch to master for lab 1 fix

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
-import { createStore } from 'redux';
-import { categories } from './components/categories/reducers';
+import { categories, CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_UPDATE, CATEGORY_REMOVE } from './components/categories/reducers';
+import { createStore, combineReducers } from 'redux';
+//import { categories } from './components/categories/reducers';
+
+const combined = combineReducers({ 
+  categories, 
+  expensesByCategory
+});
 
 const store = createStore(
-  categories,
+  combined,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
