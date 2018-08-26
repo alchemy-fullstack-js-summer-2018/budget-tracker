0c7cea961c6ef2f3f42359c0027c7b4d6e1db4b0
rbtprograms <rbt61690@gmail.com>
2018-08-26 14:02:06 -0700

    category remove working for the expense reducer

--- src/components/categories/expenses/expenseReducers.js
+++ src/components/categories/expenses/expenseReducers.js
@@ -1,10 +1,12 @@
-import { CATEGORY_LOAD, CATEGORY_ADD } from '../reducers';
+import { CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_REMOVE } from '../reducers';
 export const EXPENSE_ADD = 'EXPENSE_ADD';
 
 export const getExpenses = state => state.expensesByCategory;
 export const getExpensesById = (state, id) => getExpenses(state)[id];
 
 export function expensesByCategory(state = [], { type, payload }) {
+  console.log('**STATE**', state);
+  console.log('**PAYLOAD**', payload);
   switch(type) {
     case CATEGORY_LOAD:
       return payload.reduce((map, category) => {
@@ -16,6 +18,10 @@ export function expensesByCategory(state = [], { type, payload }) {
         ...state,
         [payload.key]: []
       };
+    case CATEGORY_REMOVE:{
+      delete state[payload];
+      return state;
+    }
     case EXPENSE_ADD:{
       return {
         ...state,
