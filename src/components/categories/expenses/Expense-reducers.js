36cb9c863b6c7bc59810a692e36a6c8323c024d7
Mario Quintana <pyrros2000@gmail.com>
2018-08-26 10:32:00 -0700

    First expense-reducer test passing

--- src/components/categories/expenses/Expense-reducers.js
+++ src/components/categories/expenses/Expense-reducers.js
@@ -1,5 +1,20 @@
 // import { CATEGORY_LOAD } from '../reducers';
-
 export const EXPENSE_ADD = 'EXPENSE_ADD';
 export const EXPENSE_UPDATE = 'EXPENSE_UPDATE';
 export const EXPENSE_REMOVE = 'EXPENSE_REMOVE';
+
+export function expenses(state = [], { type, payload }) {
+  switch(type){
+    case EXPENSE_ADD:
+      return {
+        ...state,
+        payload: payload
+      };
+    case EXPENSE_UPDATE:
+      return state.map(expense => expense.id === payload.id ? payload : expense);
+    case EXPENSE_REMOVE:
+      return state.filter(expense => expense.id !== payload);
+    default:
+      return state;
+  }
+}
