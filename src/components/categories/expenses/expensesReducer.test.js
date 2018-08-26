b23c6f1c913e7b72bad1d729774ecef789c5c591
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-26 13:30:08 -0700

    Add test for getting expenses from categories

--- src/components/categories/expenses/expensesReducer.test.js
+++ src/components/categories/expenses/expensesReducer.test.js
@@ -1,10 +1,32 @@
 import {
-  expenses
+  expenses,
+  CATEGORY_LOAD
 } from './expensesReducer';
 
 describe('Expenses reducers', () => {
   it('initializes to an empty array', () => {
     const state = expenses(undefined, {});
     expect(state).toEqual([]);
+  });
+
+  it('loads expenses from categories', () => {
+    const payload = [
+      { key: '1', expenses: [{ name: 'Netflix' }, { name: 'Hulu' }] },
+      { key: '2' },
+      { key: '3', expenses: [{ name: 'Coffee' }] }
+    ];
+
+    const result = [
+      { expenses: [{ name: 'Netflix' }, { name: 'Hulu' }], key: '1' },
+      { key: '2' },
+      { expenses: [{ name: 'Coffee' }], key: '3' }
+    ];
+
+    const state = expenses(payload, {
+      type: CATEGORY_LOAD,
+      payload
+    });
+
+    expect(state).toEqual(result);
   });
 });
