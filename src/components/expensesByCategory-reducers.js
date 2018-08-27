31d87fb7b8c2163218edc80437f323b6a16431dd
Mark Myers <markalope@gmail.com>
2018-08-27 11:18:09 -0700

    adding category cases to expenses reducer file

--- src/components/expensesByCategory-reducers.js
+++ src/components/expensesByCategory-reducers.js
@@ -1,6 +1,9 @@
+import { CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_REMOVE } from './category-reducers';
+
 export const EXPENSE_ADD = 'EXPENSE_ADD';
 export const EXPENSE_UPDATE = 'EXPENSE_UPDATE';
 export const EXPENSE_REMOVE = 'EXPENSE_REMOVE';
+
 
 export const getExpensesByCategories = state => state.expensesByCategory;
 export const getExpensesByCategoryId = (state, categoryId) => getExpensesByCategories(state)[categoryId];
@@ -8,11 +11,32 @@ export const getExpensesByCategoryId = (state, categoryId) => getExpensesByCateg
 
 export function expenses(state = [], { type, payload }) {
   switch(type) {
-    case EXPENSE_ADD:
-      return [
+
+    case CATEGORY_LOAD:
+      return payload.reduce((map, category) => {
+        map[category.id] = category.expense;
+        return map;
+      }, 
+      {});
+    case CATEGORY_ADD:
+      return {
         ...state,
-        payload
-      ];
+        [payload.id]: []
+      };
+
+    case CATEGORY_REMOVE: {
+      state[payload];
+      return state;
+    }
+
+    case EXPENSE_ADD:
+      return {
+        ...state,
+        [payload.categoryId]: [
+          ...state[payload.categoryId],
+          payload.expense
+        ]
+      };
     case EXPENSE_UPDATE:
       return state.map(expense => expense.name === payload.name ? payload : expense);
     case EXPENSE_REMOVE:
