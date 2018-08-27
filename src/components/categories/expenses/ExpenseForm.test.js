d6eeabe98474f447d3944fbed6fd3d7ee69c6c74
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-27 09:45:13 -0700

    Add test for adding expense

--- src/components/categories/expenses/ExpenseForm.test.js
+++ src/components/categories/expenses/ExpenseForm.test.js
@@ -8,5 +8,27 @@ describe('Expense Form component', () => {
     const handleComplete = jest.fn();
     const wrapper = mount(<ExpenseForm onComplete={handleComplete}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
+
+    const expense = { name: 'bus', price: 5 };
+
+    wrapper.find('input[name="name"]').simulate('change', {
+      target: {
+        name: 'name',
+        value: expense.name
+      }
+    });
+
+    wrapper.find('input[name="price"]').simulate('change', {
+      target: {
+        name: 'price',
+        value: expense.price
+      }
+    });
+
+    wrapper.find('button').simulate('submit');
+
+    const calls = handleComplete.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual(expense);
   });
 });
