0bf6eff4f6a19861aa6ea63e7cac1511a18c2d63
Mark Myers <markalope@gmail.com>
2018-08-27 16:54:50 -0700

    set up firrebase, added URL to api

--- src/services/budgetTrackerApi.js
+++ src/services/budgetTrackerApi.js
@@ -1,6 +1,6 @@
 import { put, post, get, del } from './request';
 
-const URL = 'https://.firebase.com';
+const URL = 'https://budget-tracker-61ea8.firebaseio.com';
 const CATEGORY_URL = `${URL}/categories`;
 
 const getCategoryUrl = key => `${CATEGORY_URL}/${key}.json`;
