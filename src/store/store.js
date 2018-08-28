c08f400b6ca272eb50e5cd281017ccdf913123c5
MariahAdams <mariah.adams23@yahoo.com>
2018-08-28 10:42:59 -0700

    added middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,25 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from './promise-middleware';
 import { categories } from '../components/Categories/reducers/reducers';
 import { expensesByCategory } from '../components/Expenses/reducers/expenseReducers';
+import { error, loading } from '../components/App/reducers';
 
 const combined = combineReducers({
   categories,
-  expensesByCategory
+  expensesByCategory,
+  error,
+  loading
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(
+      promiseMiddleware
+    )
+  )
 );
 
 export default store;
