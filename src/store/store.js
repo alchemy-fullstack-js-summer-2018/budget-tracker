b06d60fbf48cd75c5c9c0d24af3e750c0d509c21
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-28 10:47:41 -0700

    Restructured file organization, added promise middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,25 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from './promise-middleware';
 import { categories } from '../components/categories/reducers';
 import { expenses } from '../components/expenses/reducersExpenses';
+import { error, loading } from '../components/app/reducers';
 
 const combined = combineReducers({
   categories,
-  expenses
+  expenses,
+  error,
+  loading
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(
+      promiseMiddleware
+    )
+  )
 );
 
 export default store;
