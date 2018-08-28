48b68e414bcb45ba3d049b9293e5d5e1d70c0284
MariahAdams <mariah.adams23@yahoo.com>
2018-08-28 12:04:40 -0700

    updating expense actions

--- src/components/Expenses/reducers/expenseActions.js
+++ src/components/Expenses/reducers/expenseActions.js
@@ -1,26 +1,21 @@
 import { EXPENSE_ADD, EXPENSE_UPDATE, EXPENSE_REMOVE } from './expenseReducers';
-// import { addExpense, updateExpense, removeExpense } from '../../../services/budgetApi';
+import { addExpense, updateExpense, removeExpense } from '../../../services/budgetApi';
 // import shortid from 'shortid';
 
 export const add = (categoryId, expense) => {
-  expense.id = shortid.generate();
-  expense.timestamp = new Date();
-  expense.categoryId = categoryId;
   return {
     type: EXPENSE_ADD,
-    payload: {
-      categoryId,
-      expense
-    }
+    payload: addExpense(categoryId, expense)
   };
 };
 
 export const update = expense => ({
   type: EXPENSE_UPDATE,
-  payload: expense
+  payload: updateExpense(expense)
 });
 
+//TODO: payload: removeCategory(id).then(() => id) ?
 export const remove = expense => ({
   type: EXPENSE_REMOVE,
-  payload: expense
+  payload: removeExpense(expense)
 });
