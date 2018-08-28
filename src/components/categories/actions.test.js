8cc5535fcdbd531d4abf34c62fddb7ce6d0869a4
rbtprograms <rbt61690@gmail.com>
2018-08-27 17:56:40 -0700

    all category actions tests passing

--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -1,29 +1,18 @@
 jest.mock('../../services/categoriesApi', () => ({
   getCategories: jest.fn(),
   addCategory: jest.fn(),
-  removeCategory: jest.fn()
+  removeCategory: jest.fn(),
+  updateCategory: jest.fn()
 }));
 
 import { load, add, remove, update } from './actions';
 import { CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_REMOVE, CATEGORY_UPDATE } from './reducers';
-import { getCategories, addCategory, removeCategory } from '../../services/categoriesApi';
+import { getCategories, addCategory, removeCategory, updateCategory } from '../../services/categoriesApi';
 // import data from './categories-data';
 
 
 describe('category action creators', () => {
-
-  it('should load data', () => {
-    const promise = Promise.resolve();
-    getCategories.mockReturnValueOnce(promise);
-
-    const { type, payload } = load();
-    expect(type).toBe(CATEGORY_LOAD);
-    expect(payload).toBe(promise);
-    expect(getCategories.mock.calls.length).toBe(1);
-  });
-
-  it('should add a new category to the data', () => {
-    const category =   
+  const category =   
     { key: 'PPBqWA9', 
       name: 'Rent', 
       timeStamp: '2095-12-17T03:24:00', 
@@ -37,6 +26,18 @@ describe('category action creators', () => {
           amount: '$500',
         }
       ] };
+
+  it('should load data', () => {
+    const promise = Promise.resolve();
+    getCategories.mockReturnValueOnce(promise);
+
+    const { type, payload } = load();
+    expect(type).toBe(CATEGORY_LOAD);
+    expect(payload).toBe(promise);
+    expect(getCategories.mock.calls.length).toBe(1);
+  });
+
+  it('should add a new category to the data', () => {
     const promise = Promise.resolve();
     addCategory.mockReturnValueOnce(promise);
 
@@ -47,7 +48,7 @@ describe('category action creators', () => {
     expect(addCategory.mock.calls[0][0]).toBe(category);
   });
 
-  it.only('should remove a category from the data', () => {
+  it('should remove a category from the data', () => {
     const promise = Promise.resolve();
     removeCategory.mockReturnValueOnce(promise);
     const id = 123;
@@ -63,11 +64,27 @@ describe('category action creators', () => {
   });
 
   it('should update an item in the database', () => {
-    const payload = { key: 'GWBqNA2', 'name': 'Groceries', 'timeEntered': '1995-12-17T03:24:00', 'budget': 500 };
-    const expectedAction = {
-      type: CATEGORY_UPDATE,
-      payload
-    };
-    expect(update(payload.key).type).toEqual(expectedAction.type);
+    const updatedCategory = 
+    { key: 'PPBqWA9', 
+      name: 'HOUSING', 
+      timeStamp: '2095-12-17T03:24:00', 
+      budget: 500,
+      expenses: [
+        {
+          id: 'GQ45',
+          categoryId: 'PPBqWA9',
+          timestamp: '2018-11-10T03:24:00',
+          name: 'Rent Payment',
+          amount: '$500',
+        }
+      ] };
+    const promise = Promise.resolve();
+    updateCategory.mockReturnValueOnce(promise);
+
+    const { type, payload } = update(updatedCategory);
+    expect(type).toBe(CATEGORY_UPDATE);
+    expect(payload).toBe(promise);
+    expect(updateCategory.mock.calls.length).toBe(1);
+    expect(updateCategory.mock.calls[0][0]).toBe(updatedCategory);
   });
 });
