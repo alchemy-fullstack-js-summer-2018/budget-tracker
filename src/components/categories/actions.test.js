878bb7a33ceac792a273ff80a728768b8275640a
Mario Quintana <pyrros2000@gmail.com>
2018-08-28 08:08:39 -0700

    All category action creator tests passsing

--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -4,9 +4,9 @@ jest.mock('../../services/budgetApi', () => ({
   removeCategory: jest.fn()
 }));
 
-import { load  } from './actions';
-import { CATEGORY_LOAD } from './reducers';
-import { loadCategories } from '../../services/budgetApi';
+import { load, add, remove  } from './actions';
+import { CATEGORY_LOAD, CATEGORY_ADD, CATEGORY_REMOVE } from './reducers';
+import { loadCategories, addCategory, removeCategory } from '../../services/budgetApi';
 
 describe('Category action creators', () => {
 
@@ -18,5 +18,35 @@ describe('Category action creators', () => {
     expect(type).toBe(CATEGORY_LOAD);
     expect(payload).toBe(promise);
     expect(loadCategories.mock.calls.length).toBe(1);
+  });
+
+  it('Adds a category', () => {
+    const category = { name: 'Car' };
+    const promise = Promise.resolve();
+    addCategory.mockReturnValueOnce(promise);
+
+    const { type, payload } = add(category);
+    expect(type).toBe(CATEGORY_ADD);
+    expect(payload).toBe(promise);
+    expect(addCategory.mock.calls.length).toBe(1);
+    expect(addCategory.mock.calls[0][0]).toBe(category);
+
+  });
+
+  it('Removes a category', () => {
+    const promise = Promise.resolve();
+    removeCategory.mockReturnValueOnce(promise);
+
+    const id = 456;
+
+    const { type, payload } = remove(id);
+    expect(type).toBe(CATEGORY_REMOVE);
+    expect(removeCategory.mock.calls.length).toBe(1);
+    expect(removeCategory.mock.calls[0][0]).toBe(id);
+
+    return payload.then(idToDelete => {
+      expect(idToDelete).toBe(id);
+    });
+
   });
 });
