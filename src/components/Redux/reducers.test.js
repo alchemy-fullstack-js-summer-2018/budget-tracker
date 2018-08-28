3ac4a35b965809607ad0659387ca689679722b77
carriecarter <carrie@carrie-carter.com>
2018-08-28 12:08:42 -0700

    reducers tests 2 passing

---
+++ src/components/Redux/reducers.test.js
@@ -0,0 +1,27 @@
+import {
+  categories,
+  CATEGORY_LOAD,
+  // CATEGORY_ADD,
+  // CATEGORY_UPDATE,
+  // CATEGORY_REMOVE
+} from './reducers';
+
+describe('categories reducers', () => {
+
+  it('initializes to empty array', () => {
+    const state = categories(undefined, {});
+    expect(state).toEqual([]);
+  });
+
+  it('loads categories', () => {
+    const payload = [{}, {}, {}];
+    const state = categories([], {
+      type: CATEGORY_LOAD,
+      payload
+    });
+    expect(state).toBe(payload);
+  });
+
+
+
+});
