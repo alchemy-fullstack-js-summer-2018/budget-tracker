edfab81669d3e6c2e8b6485d77887fcf107f7a8e
Mark Myers <markalope@gmail.com>
2018-08-27 17:03:24 -0700

    built out api file

--- src/services/budgetTrackerApi.js
+++ src/services/budgetTrackerApi.js
@@ -4,3 +4,35 @@ const URL = 'https://budget-tracker-61ea8.firebaseio.com';
 const CATEGORY_URL = `${URL}/categories`;
 
 const getCategoryUrl = key => `${CATEGORY_URL}/${key}.json`;
+
+export const getCategories = () => {
+  return get(`${CATEGORY_URL}.json`)
+    .then(response => {
+      return response
+        ? Object.keys(reponse).map(key => {
+          const each = response[key];
+          each.key = key;
+          return each;
+        })
+        : [];
+    });
+};
+
+export const addCategory = (category) => {
+  const url = `${CATEGORY_URL}.json`;
+  return post(url, category)
+    .then(res => {
+      category.key = res.name;
+      return category;
+    });
+};
+
+export const updateCategory = category => {
+  const url = getCategoryUrl(category.key);
+  return put(url, category);
+};
+export const removeCategory = id => {
+  const url = getCategoryUrl(id);
+  return del(url);
+};
+        
