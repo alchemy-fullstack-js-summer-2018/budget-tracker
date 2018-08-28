51d1ea8c40f8aedfde4e238829daa1ea39bf7ad8
Rehmsy <sarahrehmer@gmail.com>
2018-08-28 09:58:07 -0700

    added some notes/code from class

--- src/components/categories/reducersExpenses.js
+++ src/components/categories/reducersExpenses.js
@@ -1,10 +1,10 @@
-import { CATEGORY_LOAD } from './reducers';
+import { CATEGORY_LOAD, CATEGORY_REMOVE } from './reducers';
 
 export const EXPENSE_ADD = 'EXPENSE_ADD';
 export const EXPENSE_UPDATE = 'EXPENSE_UPDATE';
 export const EXPENSE_REMOVE = 'EXPENSE_REMOVE';
 
-
+// state in parens from easton class demo (state)
 export const getExpensesByCategory = state => state.expensesByCategory;
 export const getExpenses = state => state.expenses;
 
@@ -25,11 +25,27 @@ export function expensesByCategory(state = [], { type, payload }) {
 
 export function expenses(state = [], { type, payload }) {
   switch(type) {
+
+    //from mariah class demo
     case EXPENSE_ADD:
       return {
         ...state,
-        payload: payload 
+        [payload.categoryId]: [
+          ...state[payload.categoryId],
+          payload.expense
+        ]
+
+        //payload: payload 
       };
+
+      //from mariah class demo. problems.
+    case CATEGORY_REMOVE: {
+      // const { [payload]: ignore, ...rest } = state;
+      // return rest;
+      const copy = { ...state };
+      delete copy[payload];
+      return copy;
+    }
       // case EXPENSE_UPDATE:
       //   return state.map(expense => expense.id === payload.id ? payload : expense);
   }
