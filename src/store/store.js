4e2795a041cb6cb8627e6f7fee4f222d036e2df6
Rehmsy <sarahrehmer@gmail.com>
2018-08-29 11:52:44 -0700

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,26 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { categories } from '../components/categories/reducers';
 import { expensesByCategory } from '../components/categories/reducersExpenses';
+import promiseMiddleware from './promise-middleware';
+import { error, loading } from '..//components/app/reducers';
+
 
 const combined = combineReducers({ 
   categories, 
-  expensesByCategory
+  expensesByCategory,
+  error,
+  loading
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(
+      promiseMiddleware
+    )
+  )
 );
 
 export default store;
