05a4a5415cab182dbad4a91d111fd10de5098301
Mario Quintana <pyrros2000@gmail.com>
2018-08-28 21:38:58 -0700

    Expenses mathcing to category id and css has been tweaked

--- src/components/categories/categories-data.js
+++ src/components/categories/categories-data.js
@@ -2,23 +2,23 @@ export default [
   // eslint-disable-next-line
   { 'key': '-LKTQHx7y2E_ZNFUEf-b', 
     'timestamp': new Date(), 
-    'name': 'Food', 
-    'budget': '200',
+    'name': 'Datsun Truck', 
+    'budget': '3000',
     'expenses': [
       {
         'id': '123',
         'categoryId': '-LKTQHx7y2E_ZNFUEf-b',
         'timestamp': new Date(), 
-        'name': 'Snack',
-        'price': '70' 
+        'name': 'engine',
+        'price': '1200' 
 
       },
       {
         'id': '456',
         'categoryId': '-LKTQHx7y2E_ZNFUEf-b',
         'timestamp': new Date(), 
-        'name': 'Produce',
-        'price': '100' 
+        'name': 'Paint job',
+        'price': '1000' 
       }
     ]
   }, 
@@ -26,15 +26,15 @@ export default [
   { 
     'key': '-LKTi4U-7iQyGe9m1Zy5', 
     'timestamp': new Date(),
-    'name': 'Car',
+    'name': 'Laptop',
     'budget': '250', 
     'expenses': [
       {
         'id': '789',
         'categoryId': '-LKTi4U-7iQyGe9m1Zy5',
         'timestamp': new Date(), 
-        'name': 'Gas',
-        'price': '500' 
+        'name': 'Dell xp5',
+        'price': '900' 
       },
     ]
   }
