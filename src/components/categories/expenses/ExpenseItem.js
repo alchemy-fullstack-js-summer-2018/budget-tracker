e96997ff9084dccf3a7573baa40a5c537c88a741
MariahAdams <mariah.adams23@yahoo.com>
2018-08-29 14:53:13 -0700

    added link to dashboard on home page

--- src/components/categories/expenses/ExpenseItem.js
+++ src/components/categories/expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { remove } from './reducers/expenseActions';
 
 class ExpenseItem extends Component {
--- src/components/categories/expenses/Expenses.js
+++ src/components/categories/expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ExpensesForm from './ExpensesForm';
 import Expense from './Expense';
 import { getExpensesByCategoryId } from './reducers/expenseReducers';
