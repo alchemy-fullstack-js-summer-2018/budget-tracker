df3c63cec27041cfef9be7522ddc0dd3048d6425
Rehmsy <sarahrehmer@gmail.com>
2018-08-29 11:41:53 -0700

    set up store folder

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from 'redux';
-import { categories } from './components/categories/reducers';
-import { expensesByCategory } from './components/categories/reducersExpenses';
+import { categories } from '../components/categories/reducers';
+import { expensesByCategory } from '../components/categories/reducersExpenses';
 
 const combined = combineReducers({ 
   categories, 
