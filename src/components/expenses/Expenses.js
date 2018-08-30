0cfc71b429b430ca4735dfcb23d14c90f1ce3c6e
carriecarter <carrie@carrie-carter.com>
2018-08-30 13:16:35 -0700

    class time

---
+++ src/components/expenses/Expenses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { add } from './actions';
+import Expense from './Expense';
+import ExpenseForm from './ExpenseForm';
+import { getExpenses } from './reducers';
+
+class Expenses extends Component {
+  
+}
